6543bb7256896d3cd18b4336f0258421c5ae7da6
Miguel <jemiguelrivas@gmail.com>
2023-05-01 05:13:54 -0700

    improving installing process
    - WIP for inserting layout config after <layoutList>

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-cp romala /usr/share/X11/xkb/symbols/romala
-echo "Romala has been added to /usr/share/X11/xkb/symbols"
+# layout=$(cat ./bin/new_layout)
+
+# sed -i "1335 i $layout" ./bin/base.xml
+# sed -i '/<layoutList>/c\$layout' ./bin/base.xml
+# sed -i "s/<layoutList>/$layout/1" ./bin/base.xml
+
+mv /usr/share/X11/xkb/rules/base.lst /usr/share/X11/xkb/rules/base.lst.copy
+mv /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/evdev.lst.copy
+mv /usr/share/X11/xkb/rules/base.xml /usr/share/X11/xkb/rules/base.xml.copy
+mv /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/evdev.xml.copy
+
+cp ./bin/base.lst /usr/share/X11/xkb/rules
+cp ./bin/evdev.lst /usr/share/X11/xkb/rules
+cp ./bin/base.xml /usr/share/X11/xkb/rules
+cp ./bin/evdev.xml /usr/share/X11/xkb/rules
+cp ./bin/romala /usr/share/X11/xkb/symbols
